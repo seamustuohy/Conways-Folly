55438d6f05908760f74a78397d17d5800da67837
s2e <s2e@s2e-ThinkPad-T60.(none)>
2012-01-17 17:57:07 -0800

    updated board and added temp evolve

--- test_conway.py
+++ test_conway.py
@@ -12,18 +12,39 @@ class check_players(unittest.TestCase):
 class board_testing(unittest.TestCase):
     def test_board_create(self):
         """compare board creation to static creation"""
-        board = conways_game.game_size(20)
+        board = conways_game.build_board_matrix(20)
         test_board = []
         for x in range(20):
             test_board.append([" "] * 20)
         self.assertEqual(test_board, board)
-
+        
     def test_tile_set(self):
         """compare modified board with original to ensure change """
-        test_board = conways_game.game_size(30)
-        test_board_two = conways_game.game_size(30)
-        test_board_two[0][0] = "x"
+        test_board = conways_game.build_board_matrix(30)
+        test_board_two = conways_game.build_board_matrix(30)
+        test_board_two[1][1] = "x"
         self.assertNotEqual(test_board, test_board_two)
+
+class phase_testing(unittest.TestCase):
+    """tests that phases correctly exicute and iterate """
+#    def test_player_iterate(self):
+#        players = ["bob", "frank", "other", "again", "really?"]
+#        finished_players = conways_game.play_phase(players)
+#        self.assertEqual(players, finished_players)
+
+class evolution_testing(unittest.TestCase):
+    """ checks evolution"""
+    def test_evolve(self):
+        a = "a"
+        b = "b"
+        board = [[" ", " ", " ", " ", " "],[" ", " ", a, a, " "],[" ", a, a, a, " "],[" ", a, " ", " ", " "],[" ", " ", " ", " ", " "]]
+        test_board = [[" ", " ", " ", " ", " "],[" ", a, " ", a, " "],[" ", a, " ", a, " "],[" ", a, " ", " ", " "],[" ", " ", " ", " ", " "]]
+        conways_game.print_board(board)
+        board = conways_game.evolve(board, 1)
+        self.assertEqual(board, test_board)
+
+        
+
 
 
         
